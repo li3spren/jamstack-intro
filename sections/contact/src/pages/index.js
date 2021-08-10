a46b487ee34ee5ce1afd2e6464977bbe9e9b37bc
Kaushal Patel <kaushal197@gmail.com>
2021-08-09 22:49:38 -0400

    feat: form markup and styling

---
+++ sections/contact/src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Form from "../components/form";
+
+import '../global.css';
+
+const Main = () => (
+  <>
+    <h1>Send a Message</h1>
+    <Form />
+  </>
+)
+
+export default Main;
